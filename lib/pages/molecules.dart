7faf6e38d7f525a06c73d6769530bc9a97b62030
sushmit1 <sushmit718@gmail.com>
2023-02-22 00:08:21 +0530

    atom: added molecule page

--- lib/pages/molecules.dart
+++ lib/pages/molecules.dart
@@ -1,15 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 import '../atom_home.dart';
 
 class Molecules extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: NavBar(),
-      appBar: AppBar(
-        title: Text('Molecules'),
-      ),
+    return MaterialApp(
+      home: Scaffold(
+          drawer: NavBar(),
+          appBar: AppBar(
+            title: Text('Molecules'),
+          ),
+          body: Container(
+              child: Center(
+            child: Container(
+              height: 400.0,
+              alignment: Alignment.center,
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    child: Image.network(
+                        'https://atom.think-digital.in/static/media/construction.fa62068b.gif'),
+                    alignment: Alignment.center,
+                    width: 400,
+                    height: 300,
+                  ),
+                  Container(
+                    child: Text(
+                      'We are currently working on this page',
+                      textAlign: TextAlign.center,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ))),
     );
   }
 }
