d02c78d2d180c08654ca2250f4fef494372958fd
sushmit1 <sushmit718@gmail.com>
2023-02-24 17:38:26 +0530

    atom: Little work on myspace

--- lib/pages/myspace.dart
+++ lib/pages/myspace.dart
@@ -9,6 +9,79 @@ class MySpace extends StatelessWidget {
       appBar: AppBar(
         title: Text('My Space'),
       ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Expanded(child: ProfileCardWidget()),
+            Expanded(child: TasksCardWidget())
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileCardWidget extends StatelessWidget {
+  const ProfileCardWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          height: 300,
+          child: Card(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0)),
+            color: Color.fromRGBO(208, 218, 221, 1),
+            child: Column(
+              children: <Widget>[
+                const ListTile(
+                  leading: Icon(Icons.view_column),
+                  title: Text('View Profile'),
+                ),
+                const CircleAvatar(
+                  radius: 50,
+                  backgroundImage: NetworkImage(
+                      'https://avatars.githubusercontent.com/u/80753594?v=4'),
+                ),
+                Text(
+                  'Sushmit',
+                  style: TextStyle(fontSize: 40),
+                ),
+                Text('sushmit718')
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TasksCardWidget extends StatelessWidget {
+  const TasksCardWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          height: 300,
+          child: Card(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0)),
+            color: Color.fromRGBO(208, 218, 221, 1),
+            child: Column(
+              children: <Widget>[
+                const ListTile(
+                  leading: Text('View Tasks'),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
